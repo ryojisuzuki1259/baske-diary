519ad1c08a541ae697ff7ab9bd7a87210c7caaf9
EC2 Default User <ec2-user@ip-172-31-88-158.ec2.internal>
2020-11-23 06:05:16 +0000

    Add models

---
+++ db/migrate/20201123042122_create_diaries.rb
@@ -0,0 +1,11 @@
+class CreateDiaries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :diaries do |t|
+
+      t.string :title, null: false
+      t.text :body, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20201123054805_create_diary_comments.rb
@@ -0,0 +1,10 @@
+class CreateDiaryComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :diary_comments do |t|
+
+      t.text :body, null: false
+
+      t.timestamps
+    end
+  end
+end
