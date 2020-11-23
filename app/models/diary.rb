820163bbaa73a2f0c41832c5241ac46694a14d80
EC2 Default User <ec2-user@ip-172-31-88-158.ec2.internal>
2020-11-23 06:39:36 +0000

    Add associations

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,2 +1,5 @@
 class Diary < ApplicationRecord
+  belongs_to :user
+  has_many :diary_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :diaries, dependent: :destroy
+  has_many :skills, dependent: :destroy
+  has_many :diary_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :events, dependent: :destroy
 end
