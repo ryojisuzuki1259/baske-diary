1cebef7e9c8a9e260b813efd8cec02e1632af403
EC2 Default User <ec2-user@ip-172-31-88-158.ec2.internal>
2020-11-23 04:17:16 +0000

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'homes#top'
+  resources :diaries , except: [:index] do
+    resources :diary_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  resources :skills , except: [:index]
+  resources :users, only: [:index, :show]
+  resources :events , except: [:index]
+
 end
