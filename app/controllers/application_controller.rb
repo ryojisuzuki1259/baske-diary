a68b4ee2e20a03097bdcd8a0f2a85f41793f68b0
EC2 Default User <ec2-user@ip-172-31-88-158.ec2.internal>
2020-11-23 08:20:40 +0000

    Add login,registration_view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  # ユーザー新規登録の項目（カラム）を追加した場合に以下を記述する
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :group, :prefecture, :introduction])
+  end
 end
