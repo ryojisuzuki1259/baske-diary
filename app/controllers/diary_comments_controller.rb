23f0c993c10d97404738135883e7cbf57ef1ab9c
EC2 Default User <ec2-user@ip-172-31-88-158.ec2.internal>
2020-11-23 06:30:39 +0000

    Add controllers

---
+++ app/controllers/diary_comments_controller.rb
@@ -0,0 +1,7 @@
+class DiaryCommentsController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+end
---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,13 @@
+class EventsController < ApplicationController
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
