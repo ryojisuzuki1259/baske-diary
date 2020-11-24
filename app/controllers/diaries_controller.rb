4e4bf63f10b4f1b41f826e5ba46fa46827e8039b
EC2 Default User <ec2-user@ip-172-31-88-158.ec2.internal>
2020-11-24 07:40:25 +0000

    Add feature-diary

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -2,7 +2,7 @@ class DiariesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user #find(params[id])だと保存後、ダイアリーのidと同じidでユーザーidを探そうとしてしまう
     @diaries = @user.diaries
   end
 
@@ -11,14 +11,37 @@ class DiariesController < ApplicationController
   end
 
   def create
+    @diary = Diary.new(diary_params)
+    @diary.user_id = current_user.id
+    if @diary.save
+      redirect_to diary_path(@diary), notice: "投稿完了しました！"
+    else
+      render 'show'
+    end
   end
 
   def edit
+    @diary = Diary.find(params[:id])
   end
 
   def update
+    @diary = Diary.find(params[:id])
+    if @diary.update(diary_params)
+      redirect_to diary_path(@diary), notice: "編集完了しました！"
+    else
+      render 'show'
+    end
   end
 
   def destroy
+    @diary = Diary.find(params[:id])
+    @diary.destroy
+    redirect_to diary_path(@diary)
   end
+
+  private
+  def diary_params
+    params.require(:diary).permit(:title, :body)
+  end
+
 end
