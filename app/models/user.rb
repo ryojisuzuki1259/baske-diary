1a9ae40d6448364e8ee490b593b5072b6608c492
EC2 Default User <ec2-user@ip-172-31-88-158.ec2.internal>
2020-11-30 06:23:43 +0000

    Add feature-search

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,30 @@ class User < ApplicationRecord
   has_many :diary_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :events, dependent: :destroy
+
+  def self.search_for(content, method, column)
+    if column == 'name'
+      if method == 'perfect'
+        User.where(name: content)
+      elsif method == 'forward'
+        User.where('name LIKE ?', content + '%')
+      elsif method == 'backward'
+        User.where('name LIKE ?', '%' + content)
+      else
+        User.where('name LIKE ?', '%' + content + '%')
+      end
+    else
+      if method == 'perfect'
+        User.where(prefecture: content)
+      elsif method == 'forward'
+        User.where('prefecture LIKE ?', content + '%')
+      elsif method == 'backward'
+        User.where('prefecture LIKE ?', '%' + content)
+      else
+        User.where('prefecture LIKE ?', '%' + content + '%')
+      end
+    end
+  end
+  #Userモデルを検索するインスタンスメソッドを定義。ビューから送られてきた引数３つによって条件分岐をする。
+  #columnで名前か活動エリアかを分岐、methodで完全一致などの検索方法を分岐、contentでどんなワードなのかを判断。
 end
