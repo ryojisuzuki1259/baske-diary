d7618ecf139b529ee54322d94aee60b12c650742
EC2 Default User <ec2-user@ip-172-31-88-158.ec2.internal>
2020-12-01 07:42:21 +0000

    Add feature-calender

---
+++ db/migrate/20201201043819_create_events.rb
@@ -0,0 +1,14 @@
+class CreateEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :events do |t|
+
+      t.integer :user_id, null: false
+      t.string :title
+      t.string :body
+      t.datetime :start_date
+      t.datetime :end_date
+
+      t.timestamps
+    end
+  end
+end
