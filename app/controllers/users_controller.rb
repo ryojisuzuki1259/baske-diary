f60c580f75a46469ecca71f379f197661ac4462a
ryojisuzuki1259 <ryoji1259@gmail.com>
2020-12-06 12:49:49 +0000

    Add slidetoggle to diary

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @diary = Diary.new
     @diaries = @user.diaries
     @skill = Skill.new
     @skills = @user.skills
